018d083a56075baf059d67d35ca7ebf070704e27
Josh Charpentier <81543042+jcharpi@users.noreply.github.com>
2025-06-25 22:09:28 -0500

    Refine MadCourses parallax layout

--- src/pages/madcourses.tsx
+++ src/pages/madcourses.tsx
@@ -30,7 +30,7 @@ function ImageCard({ card }: { card: Card }) {
   })
 
   return (
-    <section className="relative h-screen snap-start flex flex-col items-center justify-center text-center">
+    <section className="relative h-screen snap-start flex flex-col items-center justify-center">
       <Image
         src={`/icons/${card.icon}`}
         alt=""
@@ -39,33 +39,38 @@ function ImageCard({ card }: { card: Card }) {
         aria-hidden
         className="pointer-events-none absolute left-1/2 -translate-x-1/2 -translate-y-1/3 w-[30vw] h-[30vw] -z-10 opacity-75 invert"
       />
-      <div ref={ref} className="relative w-10/12 md:w-8/12 lg:w-7/12 h-3/4">
-        <Image
-          src={`/madcourses/madcourses_${card.image}.png`}
-          alt={`MadCourses ${card.image}`}
-          fill
-          className="object-contain"
-          priority={isFirstImage}
-        />
-      </div>
+      <div ref={ref} className="flex flex-col items-center">
+        <motion.div
+          style={{ y }}
+          className="relative aspect-video w-11/12 md:w-10/12 lg:w-9/12 xl:w-8/12"
+        >
+          <Image
+            src={`/madcourses/madcourses_${card.image}.png`}
+            alt={`MadCourses ${card.image}`}
+            fill
+            className="object-contain"
+            priority={isFirstImage}
+          />
+        </motion.div>
 
-      <motion.div
-        style={{ y }}
-        className="mt-12 text-8xl font-bold tracking-tight text-white pointer-events-none select-none"
-      >
-        {inView && (
-          <div>
-            <Typewriter
-              lines={card.titleLines}
-              speed={35}
-              bold={[card.titleLines[0]]}
-            />
-            <div className="text-4xl font-medium mt-4 text-neutral-100">
-              <Typewriter lines={card.descLines} speed={5} bold={card.bold} />
+        <motion.div
+          style={{ y }}
+          className="mt-12 text-8xl font-bold tracking-tight text-white pointer-events-none select-none text-right"
+        >
+          {inView && (
+            <div>
+              <Typewriter
+                lines={card.titleLines}
+                speed={35}
+                bold={[card.titleLines[0]]}
+              />
+              <div className="text-4xl font-medium mt-4 text-neutral-100">
+                <Typewriter lines={card.descLines} speed={5} bold={card.bold} />
+              </div>
             </div>
-          </div>
-        )}
-      </motion.div>
+          )}
+        </motion.div>
+      </div>
     </section>
   )
 }
